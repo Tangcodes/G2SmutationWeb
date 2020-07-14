31609299ba7c0740069972f12b14ba75f2ee2992
Tangcodes <tangjingxuan0612@gmail.com>
2020-07-14 00:20:41 -0500

    G2SmutationWeb

---
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/domain/MutationRepository.java
@@ -0,0 +1,29 @@
+package org.cbioportal.G2Smutation.web.domain;
+
+import org.cbioportal.G2Smutation.web.database.mutation_usage_table;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+
+public interface MutationRepository extends JpaRepository<mutation_usage_table, Integer> {
+	List<mutation_usage_table> findTop10BymutationIdGreaterThan(Integer id);
+	
+	List<mutation_usage_table> findByseqNameContaining(String id);
+	
+	List<mutation_usage_table> findByseqIndex(String id);
+	List<mutation_usage_table> findBypdbIndex(String id);
+	List<mutation_usage_table> findByseqIndexAndPdbIndex(String id1,String id2);
+	
+	List<mutation_usage_table> findByseqResidue(String id);
+	List<mutation_usage_table> findBypdbResidue(String id);
+	List<mutation_usage_table> findByseqResidueAndPdbResidue(String id1,String id2);
+	
+	List<mutation_usage_table> findBypdbNoStartingWith(String id);
+	
+	
+	@Query(value = "select * from mutation_usage_table where PDB_NO LIKE '?1%' And SEQ_INDEX = ?2", nativeQuery = true)
+	List<mutation_usage_table> findBypdbNoAndPdbIndex(String id1,Integer id2);
+	List<mutation_usage_table> findByalignmentId(Integer id);
+}
+
---
+++ G2Smutation-web/src/main/java/org/cbioportal/G2Smutation/web/models/Database.java
@@ -0,0 +1,10 @@
+package org.cbioportal.G2Smutation.web.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class Database {
+
+}
